b206e6f799f0badc9f60758566e2b8ce72ed869c
Philip Hamner <philiphamner@gmail.com>
2020-11-18 16:21:57 -0800

    first commit

---
+++ src/Board.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>This is the board</h1>
+      </div>
+    );
+  }
+}
+
+export default Board;
+  
